34bd234c0bf2585e15a6b7da8f39371c9f5c96b2
MATC <MATC>
2015-11-11 10:18:31 -0700

    two controllers

--- _js/app.js
+++ _js/app.js
@@ -17,10 +17,32 @@
     }
     ];
 
+    var vocab = [
+        {
+            name: 'Directives',
+            data: "HTML annotations that trigger Javascript behaviors",
+    }, {
+            name: 'Modules',
+            data: "Where our application components live",
+    },
+        {
+            name: 'Controllers',
+            data: "Where we add application behavior",
+    },
+        {
+            name: 'Expressions',
+            data: "How values get displayed within the page",
+    },
+    ];
+
     var app = angular.module('gemStore', []);
 
     app.controller('StoreController', function () {
         this.products = gems;
+    });
+
+    app.controller('LessonController', function () {
+        this.vocab = vocab;
     });
 
 })();
